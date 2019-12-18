f556dbcd7a93499c56b71a89b238d04bda089893
gsnlyd <48926197+gsnlyd@users.noreply.github.com>
2019-12-17 19:04:34 -0500

    Clean up detector

--- inference/detect.py
+++ inference/detect.py
@@ -12,82 +12,27 @@ from torchvision.transforms import ToTensor
 
 from training import models
 
+COCO_LABELS_FILE_PATH = 'coco_labels.txt'
 DEFAULT_DETECTION_THRESHOLD = 0.7
-ALLOWED_LABELS = [
-    1,  # Person
-    2,  # Bicycle
-    3,  # Car
-    4,  # Motorcycle
-    6,  # Bus
-    8,  # Truck
-]
 
-FINETUNED_LABELS = [
-    'person',
-    'bicycle',
-    'car',
-    'motorcycle',
-    'bus',
-    'truck'
-]
-
-LABEL_COLORS = {
-    1: 'pink',
-    2: 'green',
-    3: 'lightblue',
-    4: 'orange',
-    6: 'yellow',
-    8: 'purple'
+COCO_TRAFFIC_LABELS = {
+    1: 'person',
+    2: 'bicycle',
+    3: 'car',
+    4: 'motorcycle',
+    6: 'bus',
+    8: 'truck'
 }
 
-finetuned_model: bool = False
-_model: Optional[FasterRCNN] = None
-_labels: Optional[List[str]] = None
-
-img_transform = ToTensor()
-
-
-def get_model(load_finetuned: str = None) -> Module:
-    global _model
-    if _model is not None:
-        return _model
-
-    if load_finetuned is None:
-        _model = torchvision.models.detection.fasterrcnn_resnet50_fpn(pretrained=True)
-    else:
-        _model = models.get_model(len(ALLOWED_LABELS))
-        _model.load_state_dict(torch.load(load_finetuned, map_location=torch.device('cpu')))
-        global finetuned_model
-        finetuned_model = True
-
-    _model.eval()
-
-    return _model
-
-
-def get_label_str(label: int) -> str:
-    if finetuned_model:
-        return FINETUNED_LABELS[label - 1]
-
-    global _labels
-    if _labels is None:
-        _labels = []
-        with open('coco_labels.txt') as labels_file:
-            for l in labels_file:
-                label_str = l.strip('\n')
-                _labels.append(label_str)
-
-    return _labels[label - 1]
-
-
-def get_color(label: int) -> str:
-    if finetuned_model:
-        return list(LABEL_COLORS.values())[label - 1]
-
-    if label in LABEL_COLORS:
-        return LABEL_COLORS[label]
-    else:
-        return 'white'
+LABEL_COLORS = {
+    'person': 'pink',
+    'bicycle': 'green',
+    'car': 'lightblue',
+    'motorcycle': 'orange',
+    'bus': 'yellow',
+    'truck': 'purple'
+}
+DEFAULT_COLOR = 'white'
 
 
 class Detection(NamedTuple):
@@ -97,16 +42,73 @@ class Detection(NamedTuple):
     score: float
 
 
-def visualize_detections(img: Image.Image, detections: List[Detection], show: bool = False) -> Image.Image:
-    viz_img = img.copy()
-    draw = ImageDraw.Draw(viz_img)
+class Detector:
+    def __init__(self, finetuned_path: str = None, threshold: float = DEFAULT_DETECTION_THRESHOLD,
+                 filter_traffic_detections: bool = True):
+        if finetuned_path is None:
+            self.model = torchvision.models.detection.fasterrcnn_resnet50_fpn(pretrained=True)
+        else:
+            self.model = models.get_model(len(COCO_TRAFFIC_LABELS))
+            self.model: FasterRCNN
+            self.model.load_state_dict(torch.load(finetuned_path, map_location=torch.device('cpu')))
 
+        self.model.eval()
+        self.to_tensor = ToTensor()
+
+        self.finetuned = finetuned_path is not None
+        self.threshold = threshold
+        self.filter_traffic_detections = not self.finetuned and filter_traffic_detections
+
+        self.all_coco_labels = []
+        if not self.filter_traffic_detections:
+            with open(COCO_LABELS_FILE_PATH) as coco_file:
+                for line in coco_file:
+                    self.all_coco_labels.append(line.strip('\n'))
+
+    def __label_properties(self, label: int) -> Tuple[int, str]:
+        """Returns label and label str"""
+        if not self.filter_traffic_detections:
+            # Return properties for all COCO labels
+            return label, self.all_coco_labels[label - 1]
+
+        if not self.finetuned:
+            # Re-map COCO labels to traffic labels
+            label = list(COCO_TRAFFIC_LABELS.keys()).index(label) + 1
+
+        return label, list(COCO_TRAFFIC_LABELS.values())[label - 1]
+
+    def detect_objects(self, img: Image.Image) -> List[Detection]:
+        img_tensor: Tensor = self.to_tensor(img).unsqueeze(0)  # Unsqueeze for a batch size of 1
+
+        with torch.no_grad():
+            result = self.model(img_tensor)[0]  # Result also has batch size of 1
+
+        detections: List[Detection] = []
+        for box, label, score in zip(result['boxes'], result['labels'], result['scores']):
+            label = label.item()
+            if score >= self.threshold and (not self.filter_traffic_detections or label in COCO_TRAFFIC_LABELS):
+                box = tuple(box.tolist())
+                score = score.item()
+
+                label, label_str = self.__label_properties(label)
+                detections.append(Detection(
+                    box=box,
+                    label=label,
+                    label_str=label_str,
+                    score=score
+                ))
+
+        return detections
+
+
+def visualize(img: Image.Image, detections: List[Detection]):
+    draw = ImageDraw.Draw(img)
     box_line_width = 3
 
     for d in detections:
         text = 'Label: {} ({}), Score: {:.2f}'.format(d.label_str, d.label, d.score)
 
-        color = get_color(d.label)
+        color = LABEL_COLORS[d.label_str] if d.label_str in LABEL_COLORS else DEFAULT_COLOR
         draw.rectangle(d.box, outline=color, width=box_line_width)
 
         text_width, text_height = draw.textsize(text)
@@ -123,65 +125,13 @@ def visualize_detections(img: Image.Image, detections: List[Detection], show: bo
         draw.rectangle(text_background_box, fill='black')
         draw.text(text_coords, text=text, fill=color)
 
-    if show:
-        viz_img.show()
-    return viz_img
-
-
-def detect_objects(image_path: str, threshold: float = DEFAULT_DETECTION_THRESHOLD, disable_label_filter: bool = False,
-                   print_detections: bool = False, visualize: bool = False, show_visualization: bool = False) -> \
-        Tuple[List[Detection], Optional[Image.Image]]:
-    start_time = time.time()
-
-    img = Image.open(image_path)
-    img_tensor: Tensor = img_transform(img)
-    img_tensor = img_tensor.unsqueeze(0)  # Unsqueeze for a batch size of 1
-
-    model = get_model()
-    with torch.no_grad():
-        result = model(img_tensor)[0]  # Result also has batch size of 1
-
-    detections: List[Detection] = []
-
-    for box, label, score in zip(result['boxes'], result['labels'], result['scores']):
-        if score < threshold or (not disable_label_filter and label not in ALLOWED_LABELS):
-            continue
-
-        box = (
-            box[0].item(),
-            box[1].item(),
-            box[2].item(),
-            box[3].item()
-        )
-
-        label = label.item()
-        score = score.item()
-
-        detections.append(Detection(
-            box=box,
-            label=label,
-            label_str=get_label_str(label),
-            score=score
-        ))
-
-    if print_detections:
-        for d in detections:
-            print(d)
-        print('Detection took {:.2f} seconds'.format(time.time() - start_time))
-
-    if visualize:
-        viz_img = visualize_detections(img, detections, show=show_visualization)
-        return detections, viz_img
-
-    return detections, None
-
 
 if __name__ == '__main__':
     parser = ArgumentParser()
     parser.add_argument('--image-path', '-i', type=str, required=True, help='Image on which to perform detection.')
     parser.add_argument('--threshold', '-t', type=float, default=DEFAULT_DETECTION_THRESHOLD,
                         help='Score threshold for detections.')
-    parser.add_argument('--disable-label-filter', '-df', action='store_true',
+    parser.add_argument('--disable-traffic-filter', '-df', action='store_true',
                         help='Disable the filtering of unrelated categories.')
     parser.add_argument('--finetuned-path', '-f', type=str, default=None,
                         help='Path to load fine-tuned model weights.')
@@ -189,14 +139,13 @@ if __name__ == '__main__':
     args = parser.parse_args()
     print(args)
 
-    if args.finetuned_path is not None:
-        get_model(args.finetuned_path)
+    detector = Detector(finetuned_path=args.finetuned_path,
+                        threshold=args.threshold,
+                        filter_traffic_detections=not args.disable_traffic_filter)
 
-    detect_objects(
-        image_path=args.image_path,
-        threshold=args.threshold,
-        disable_label_filter=args.disable_label_filter,
-        print_detections=True,
-        visualize=True,
-        show_visualization=True
-    )
+    im = Image.open(args.image_path)
+    det = detector.detect_objects(im)
+    for de in det:
+        print(de)
+    visualize(im, det)
+    im.show()
